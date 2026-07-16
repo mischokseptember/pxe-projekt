85489758f0b107cd076b0cbd455e083ecc201078
Ingo Blechschmidt <iblech@speicherleck.de>
2026-07-16 09:35:33 +0200

    Netzwerkinterface konfigurierbar gemacht

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# Beispielaufruf: ./run.sh eth0
+# - Statt "eth0" muss der korrekte Name des physischen
+#   Netzwerkinterface angegeben werden.
 
 set -x -euo pipefail
 
 # Name des physischen Kabelinterface
-export PHYS_INTERFACE=enp0s31f6
+export PHYS_INTERFACE=${1:-enp0s31f6}
 
 ./network.sh
